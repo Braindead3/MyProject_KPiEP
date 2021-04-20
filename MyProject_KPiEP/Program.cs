daee65deefb51d5bbea5e445361f18007c4abbd1
malosay21@gmail.com <malosay21@gmail.com>
2021-04-20 22:34:27 +0300

    a

--- MyProject_KPiEP/Program.cs
+++ MyProject_KPiEP/Program.cs
@@ -33,16 +33,35 @@ namespace MyProject_KPiEP
                 switch (messege.Text)
                 {
                     case "sticker":
-                        await client.SendStickerAsync(chatId: messege.Chat.Id,sticker: "https://tlgrm.ru/_/stickers/ff6/4b6/ff64b611-aa7c-3603-b73c-7cd86d4b71dc/10.webp", replyToMessageId: messege.MessageId,replyMarkup: GetButtons() );// отправка стикера на сообщение пользователя, ссылка это стикер(скопируй ссылку на стикер с сайта)
+                        await client.SendStickerAsync(
+                            chatId: messege.Chat.Id,
+                            sticker: "https://tlgrm.ru/_/stickers/ff6/4b6/ff64b611-aa7c-3603-b73c-7cd86d4b71dc/10.webp",
+                            replyToMessageId: messege.MessageId,
+                            replyMarkup: GetButtons() 
+                            );// отправка стикера на сообщение пользователя, ссылка это стикер(скопируй ссылку на стикер с сайта)
+
                         break;
                     case "image":
-                        await client.SendPhotoAsync(chatId: messege.Chat.Id, photo: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fcdn.eso.org%2Fimages%2Fthumb300y%2Feso1907a.jpg&imgrefurl=https%3A%2F%2Fwww.eso.org%2Fpublic%2Fimages%2Farchive%2Fzoomable%2F&tbnid=nqk2hAE034Z3nM&vet=12ahUKEwiW3tODvo3wAhUZhaQKHencAtoQMyggegUIARD5AQ..i&docid=KeUcu23CqZzwAM&w=515&h=300&q=image&ved=2ahUKEwiW3tODvo3wAhUZhaQKHencAtoQMyggegUIARD5AQ",replyMarkup: GetButtons());
+                        await client.SendPhotoAsync
+                            (
+                            chatId: messege.Chat.Id,
+                            photo: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fcdn.eso.org%2Fimages%2Fthumb300y%2Feso1907a.jpg&imgrefurl=https%3A%2F%2Fwww.eso.org%2Fpublic%2Fimages%2Farchive%2Fzoomable%2F&tbnid=nqk2hAE034Z3nM&vet=12ahUKEwiW3tODvo3wAhUZhaQKHencAtoQMyggegUIARD5AQ..i&docid=KeUcu23CqZzwAM&w=515&h=300&q=image&ved=2ahUKEwiW3tODvo3wAhUZhaQKHencAtoQMyggegUIARD5AQ",
+                            replyMarkup: GetButtons()
+                            );
                         break;
                     case "menu":
-                        await client.SendTextMessageAsync(messege.Chat.Id,messege.Text,replyMarkup : GetButtons());
+                        await client.SendTextMessageAsync(
+                            messege.Chat.Id,
+                            messege.Text,
+                            replyMarkup : GetButtons()
+                            );
                         break;
                     default:
-                        await client.SendTextMessageAsync(messege.Chat.Id, $"И че мне с этим делать",replyToMessageId: messege.MessageId);//отвечает на сообщение пользователя test
+                        await client.SendTextMessageAsync(
+                            messege.Chat.Id,
+                            $"И че мне с этим делать",
+                            replyToMessageId: messege.MessageId
+                            );//отвечает на сообщение пользователя test
                         break;
                 }
             }
