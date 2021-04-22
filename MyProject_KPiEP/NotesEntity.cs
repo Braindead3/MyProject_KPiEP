9f1262ef07313d23289ae855e2cb598d39220622
malosay21@gmail.com <malosay21@gmail.com>
2021-04-22 20:47:23 +0300

    asd

--- MyProject_KPiEP/NotesEntity.cs
+++ MyProject_KPiEP/NotesEntity.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace MyProject_KPiEP
@@ -8,5 +9,7 @@ namespace MyProject_KPiEP
     {
         public int Id { get; set; }
         public string Text { get; set; }
+        public int IdChat { get; set; }
+        public string NoteName { get; set; }
     }
 }
--- MyProject_KPiEP/Program.cs
+++ MyProject_KPiEP/Program.cs
@@ -13,33 +13,35 @@ namespace MyProject_KPiEP
     {
         static string BotToken { get; set; } = "1713856634:AAEtV-XyEnqhmJAcNCOfLedlvxSjj5cftz8";
         static TelegramBotClient client;
+        static bool addNote = false;
+        static NotesEntity note= new NotesEntity();
         static void Main(string[] args)
         {
-            //client = new TelegramBotClient(BotToken);
+            client = new TelegramBotClient(BotToken);
 
-            //client.StartReceiving();
+            client.StartReceiving();
 
-            //client.OnMessage += Client_OnMessage;
-            //client.OnCallbackQuery += Client_OnCallbackQuery;
+            client.OnMessage += Client_OnMessage;
+            client.OnCallbackQuery += Client_OnCallbackQuery;
 
-            //Console.ReadKey();
+            Console.ReadKey();
 
-            //client.StopReceiving();
+            client.StopReceiving();
 
 
-            using (var dataSource = new DataContext())
-            {
-                dataSource.Notes.Add(new NotesEntity { Text="vlad1" });
-                dataSource.Notes.Add(new NotesEntity { Text="vlad2" });
-                dataSource.SaveChanges();
+            //using (var dataSource = new DataContext())
+            //{
+            //    dataSource.Notes.Add(new NotesEntity { Text="vlad1" });
+            //    dataSource.Notes.Add(new NotesEntity { Text="vlad2" });
+            //    dataSource.SaveChanges();
 
-                var notes = dataSource.Notes.ToList();
+            //    var notes = dataSource.Notes.ToList();
 
-                foreach (var note in notes)
-                {
-                    Console.WriteLine(note.Text);
-                }
-            }
+            //    foreach (var note in notes)
+            //    {
+            //        Console.WriteLine(note.Text);
+            //    }
+            //}
         }
 
         private static async void Client_OnCallbackQuery(object sender, Telegram.Bot.Args.CallbackQueryEventArgs e)
@@ -62,23 +64,23 @@ namespace MyProject_KPiEP
             {
                 switch (messege.Text)
                 {
-                    case "sticker":
-                        await client.SendStickerAsync(
-                            chatId: messege.Chat.Id,
-                            sticker: "https://tlgrm.ru/_/stickers/ff6/4b6/ff64b611-aa7c-3603-b73c-7cd86d4b71dc/10.webp",
-                            replyToMessageId: messege.MessageId,
-                            replyMarkup: GetButtons() 
-                            );// отправка стикера на сообщение пользователя, ссылка это стикер(скопируй ссылку на стикер с сайта)
+                    //case "sticker":
+                    //    await client.SendStickerAsync(
+                    //        chatId: messege.Chat.Id,
+                    //        sticker: "https://tlgrm.ru/_/stickers/ff6/4b6/ff64b611-aa7c-3603-b73c-7cd86d4b71dc/10.webp",
+                    //        replyToMessageId: messege.MessageId,
+                    //        replyMarkup: GetButtons() 
+                    //        );// отправка стикера на сообщение пользователя, ссылка это стикер(скопируй ссылку на стикер с сайта)
 
-                        break;
-                    case "image":
-                        await client.SendPhotoAsync
-                            (
-                            chatId: messege.Chat.Id,
-                            photo: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fcdn.eso.org%2Fimages%2Fthumb300y%2Feso1907a.jpg&imgrefurl=https%3A%2F%2Fwww.eso.org%2Fpublic%2Fimages%2Farchive%2Fzoomable%2F&tbnid=nqk2hAE034Z3nM&vet=12ahUKEwiW3tODvo3wAhUZhaQKHencAtoQMyggegUIARD5AQ..i&docid=KeUcu23CqZzwAM&w=515&h=300&q=image&ved=2ahUKEwiW3tODvo3wAhUZhaQKHencAtoQMyggegUIARD5AQ",
-                            replyMarkup: GetButtons()
-                            );
-                        break;
+                    //    break;
+                    //case "image":
+                    //    await client.SendPhotoAsync
+                    //        (
+                    //        chatId: messege.Chat.Id,
+                    //        photo: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fcdn.eso.org%2Fimages%2Fthumb300y%2Feso1907a.jpg&imgrefurl=https%3A%2F%2Fwww.eso.org%2Fpublic%2Fimages%2Farchive%2Fzoomable%2F&tbnid=nqk2hAE034Z3nM&vet=12ahUKEwiW3tODvo3wAhUZhaQKHencAtoQMyggegUIARD5AQ..i&docid=KeUcu23CqZzwAM&w=515&h=300&q=image&ved=2ahUKEwiW3tODvo3wAhUZhaQKHencAtoQMyggegUIARD5AQ",
+                    //        replyMarkup: GetButtons()
+                    //        );
+                    //    break;
                     case "menu":
                         await client.SendTextMessageAsync(
                             messege.Chat.Id,
@@ -86,6 +88,15 @@ namespace MyProject_KPiEP
                             replyMarkup : GetButtons()
                             );
                         break;
+                    case "Добавить заметку":
+                        {
+                            await client.SendTextMessageAsync(
+                                messege.Chat.Id,
+                                "Введите имя заметки"
+                                );
+                            addNote = true;
+                            break;
+                        }
                     case "test":
                         await client.SendTextMessageAsync(
                             messege.Chat.Id,
@@ -93,11 +104,19 @@ namespace MyProject_KPiEP
                             );
                         break;
                     default:
-                        await client.SendTextMessageAsync(
-                            messege.Chat.Id,
-                            $"И че мне с этим делать",
-                            replyToMessageId: messege.MessageId
-                            );//отвечает на сообщение пользователя test
+                        if (addNote != false)
+                        {
+                            note.NoteName = messege.Text;
+                            note.IdChat =Convert.ToInt32(messege.Chat.Id);
+                        }
+                        else
+                        {
+                            await client.SendTextMessageAsync(
+                                messege.Chat.Id,
+                                $"И че мне с этим делать",
+                                replyToMessageId: messege.MessageId
+                                );//отвечает на сообщение пользователя test
+                        }
                         break;
                 }
             }
@@ -114,7 +133,7 @@ namespace MyProject_KPiEP
             {
                 Keyboard = new List<List<KeyboardButton>>
                 {
-                    new List<KeyboardButton> { new KeyboardButton { Text = "sticker" }, new KeyboardButton { Text = "image" }, new KeyboardButton {Text = "test" } }
+                    new List<KeyboardButton> { new KeyboardButton { Text = "Добавить заметку" }, new KeyboardButton { Text = "Удалить заметку" }, new KeyboardButton {Text = "Редактировать заметку" } }
                 }
             };
         }
